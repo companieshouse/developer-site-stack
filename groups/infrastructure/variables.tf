a0670b8bf90b04c2304a0cae93764a699995683d
kbodige <kbodige>
2023-04-11 13:13:30 +0100

    updated default value

--- groups/infrastructure/variables.tf
+++ groups/infrastructure/variables.tf
@@ -105,6 +105,7 @@ variable "internal_albs" {
 
 # Container Insights - ECS
 variable "enable_container_insights" {
-  type        = string
-  description = "Whether container sights are set, valid values are [enabled,disabled]" 
+  type        = bool
+  description = "Whether container sights are set, valid values are [enabled,disabled]"
+  default     = false 
 }
