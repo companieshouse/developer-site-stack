e950fec61987a34af40039fac32cf654b78b8bc9
kbodige <kbodige>
2023-04-11 13:04:24 +0100

    updated type to string

--- groups/infrastructure/variables.tf
+++ groups/infrastructure/variables.tf
@@ -105,6 +105,6 @@ variable "internal_albs" {
 
 # Container Insights - ECS
 variable "enable_container_insights" {
-  type        = bool
+  type        = string
   description = "Whether container sights are set, valid values are [enabled,disabled]" 
 }
