fdebb053e7f0af2b0cce9370b5297c663495c70a
kbodige <kbodige>
2023-04-11 14:16:51 +0100

    To test

--- groups/infrastructure/main.tf
+++ groups/infrastructure/main.tf
@@ -96,8 +96,7 @@ locals {
 }
 
 module "ecs-cluster" {
-  source = "git::git@github.com:companieshouse/terraform-library-ecs-cluster.git?ref=1.1.4"
-  #source = "git@github.com:companieshouse/terraform-library-ecs-cluster.git"
+  source = "git@github.com:companieshouse/terraform-library-ecs-cluster.git"
 
   stack_name                    = local.stack_name
   name_prefix                   = local.name_prefix
