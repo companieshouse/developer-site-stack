914c1c8a0b6e993bbe320f8fd751f66198eb3dee
kbodige <kbodige>
2023-04-11 13:23:22 +0100

    removed value that is commented

--- groups/infrastructure/main.tf
+++ groups/infrastructure/main.tf
@@ -108,7 +108,6 @@ module "ecs-cluster" {
   ec2_image_id                  = var.ec2_image_id
   asg_max_instance_count        = var.asg_max_instance_count
   asg_min_instance_count        = var.asg_min_instance_count
-  #enable_container_insights     = var.enable_container_insights
   asg_desired_instance_count    = var.asg_desired_instance_count
 }
 
