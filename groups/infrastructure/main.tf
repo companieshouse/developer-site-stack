c0cada3f9687ae879f53ba2acda06d23e8e9165e
kbodige <kbodige>
2023-04-12 10:26:30 +0100

    Source URL updated

--- groups/infrastructure/main.tf
+++ groups/infrastructure/main.tf
@@ -96,7 +96,7 @@ locals {
 }
 
 module "ecs-cluster" {
-  source = "git@github.com:companieshouse/terraform-library-ecs-cluster.git"
+  source = "git@github.com:companieshouse/terraform-library-ecs-cluster.git?ref=feature/CC-166-ContainerInsights"
 
   stack_name                    = local.stack_name
   name_prefix                   = local.name_prefix
