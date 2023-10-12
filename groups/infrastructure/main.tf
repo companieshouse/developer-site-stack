87d8caf8a4db11429413e1c6bddc9127b4d32a96
Ivana Pavic <ipavic@companieshouse.gov.uk>
2023-10-12 12:16:57 +0100

    Upgrading to tf 1.3, refactoring

--- groups/infrastructure/main.tf
+++ groups/infrastructure/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {}
-  required_version = "~> 0.13"
+  required_version = "~> 1.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -17,91 +17,14 @@ terraform {
   }
 }
 
-# Configure the remote state data source to acquire configuration
-# created through the code in ch-service-terraform/aws-mm-networks.
-data "terraform_remote_state" "networks" {
-  backend = "s3"
-  config = {
-    bucket = var.remote_state_bucket
-    key    = "${var.state_prefix}/${var.deploy_to}/${var.deploy_to}.tfstate"
-    region = var.aws_region
-  }
-}
-locals {
-  vpc_id            = data.terraform_remote_state.networks.outputs.vpc_id
-  application_ids   = data.terraform_remote_state.networks.outputs.application_ids
-  application_cidrs = data.terraform_remote_state.networks.outputs.application_cidrs
-  public_ids        = data.terraform_remote_state.networks.outputs.public_ids
-  public_cidrs      = data.terraform_remote_state.networks.outputs.public_cidrs
-}
-
-# Configure the remote state data source to acquire configuration created
-# through the code in aws-common-infrastructure-terraform/groups/networking.
-data "terraform_remote_state" "networks_common_infra" {
-  backend = "s3"
-  config = {
-    bucket = var.aws_bucket
-    key    = "aws-common-infrastructure-terraform/common-${var.aws_region}/networking.tfstate"
-    region = var.aws_region
-  }
-}
-locals {
-  internal_cidrs = values(data.terraform_remote_state.networks_common_infra.outputs.internal_cidrs)
-  vpn_cidrs      = values(data.terraform_remote_state.networks_common_infra.outputs.vpn_cidrs)
-}
-
-# Remote state data source for Ireland, required for Concourse management CIDRs
-data "terraform_remote_state" "networks_common_infra_ireland" {
-  backend = "s3"
-  config = {
-    bucket = "development-eu-west-1.terraform-state.ch.gov.uk"
-    key    = "aws-common-infrastructure-terraform/common-eu-west-1/networking.tfstate"
-    region = "eu-west-1"
-  }
-}
-locals {
-  management_private_subnet_cidrs = values(data.terraform_remote_state.networks_common_infra_ireland.outputs.management_private_subnet_cidrs)
-}
-
-# Configure the remote state data source to acquire configuration
-# created through the code in the services-stack-configs stack in the
-# aws-common-infrastructure-terraform repo.
-data "terraform_remote_state" "services-stack-configs" {
-  backend = "s3"
-  config = {
-    bucket = var.aws_bucket # aws-common-infrastructure-terraform repo uses the same remote state bucket
-    key    = "aws-common-infrastructure-terraform/common-${var.aws_region}/services-stack-configs.tfstate"
-    region = var.aws_region
-  }
-}
-
-data "vault_generic_secret" "secrets" {
-  path = "applications/${var.aws_profile}/${var.environment}/${local.stack_fullname}"
-}
-
-locals {
-  # stack name is hardcoded here in main.tf for this stack. It should not be overridden per env
-  stack_name     = "developer-site"
-  stack_fullname = "${local.stack_name}-stack"
-  name_prefix    = "${local.stack_name}-${var.environment}"
-
-  public_lb_cidrs    = ["0.0.0.0/0"]
-  iboss_access_cidrs = ["10.40.250.0/24"]
-
-  lb_subnet_ids = var.internal_albs ? local.application_ids : local.public_ids # place ALB in correct subnets
-  lb_access_cidrs = (var.internal_albs ?
-    concat(local.internal_cidrs, local.vpn_cidrs, local.management_private_subnet_cidrs, local.iboss_access_cidrs, split(",", local.application_cidrs)) :
-  local.public_lb_cidrs)
-}
-
 module "ecs-cluster" {
   source = "git@github.com:companieshouse/terraform-library-ecs-cluster.git?ref=1.1.4"
 
   stack_name                 = local.stack_name
   name_prefix                = local.name_prefix
   environment                = var.environment
-  vpc_id                     = local.vpc_id
-  subnet_ids                 = local.application_ids
+  vpc_id                     = data.aws_vpc.vpc.id
+  subnet_ids                 = local.application_subnet_ids
   ec2_key_pair_name          = var.ec2_key_pair_name
   ec2_instance_type          = var.ec2_instance_type
   ec2_image_id               = var.ec2_image_id
@@ -112,13 +35,12 @@ module "ecs-cluster" {
 }
 
 module "secrets" {
-  source = "./module-secrets"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.192"
 
-  stack_name  = local.stack_name
-  name_prefix = local.name_prefix
   environment = var.environment
-  kms_key_id  = data.terraform_remote_state.services-stack-configs.outputs.services_stack_configs_kms_key_id
-  secrets     = data.vault_generic_secret.secrets.data
+  name_prefix = local.name_prefix
+  secrets     = local.parameter_store_secrets
+  kms_key_id  = data.aws_kms_key.stack_configs.id
 }
 
 module "albs" {
@@ -127,7 +49,7 @@ module "albs" {
   stack_name                = local.stack_name
   name_prefix               = local.name_prefix
   environment               = var.environment
-  vpc_id                    = local.vpc_id
+  vpc_id                    = data.aws_vpc.vpc.id
   ssl_certificate_id        = var.ssl_certificate_id
   zone_id                   = var.zone_id
   external_top_level_domain = var.external_top_level_domain
