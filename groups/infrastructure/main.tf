6a2d2e519684e9891a044c89e07de1500439074a
Harish Marimuthu <hmarimuthu@companieshouse.gov.uk>
2024-04-18 14:56:36 +0100

    CC-1256 Testing a release.

--- groups/infrastructure/main.tf
+++ groups/infrastructure/main.tf
@@ -68,15 +68,6 @@ resource "aws_security_group" "temporary_to_delete" {
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = data.aws_vpc.vpc.id
 
-  ingress = []
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
   tags = {
     Name = "temporary_to_delete"
   }
