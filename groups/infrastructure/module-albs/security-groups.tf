d8f16dde2aaf2a5d525aea11be7732ef54c75c1b
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2026-06-08 08:22:25 +0100

    Refactor security group rules, add supporting module var

--- groups/infrastructure/module-albs/security-groups.tf
+++ groups/infrastructure/module-albs/security-groups.tf
@@ -2,29 +2,67 @@ resource "aws_security_group" "dev-site-sg" {
   description = "Security group for dev site albs"
   vpc_id      = var.vpc_id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = var.web_access_cidrs
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = var.web_access_cidrs
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  ingress = []
+  egress  = []
 
   tags = {
     Environment = var.environment
     Name        = "${var.name_prefix}-alb-sg"
   }
+}
+
+resource "aws_vpc_security_group_egress_rule" "all" {
+  security_group_id = aws_security_group.dev-site-sg.id
+
+  description = "Permit egress access to all"
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http_cidrs" {
+  for_each = toset(var.web_access_cidrs)
+
+  security_group_id = aws_security_group.dev-site-sg.id
+
+  description = "Permit HTTP access from ${each.value}"
+  cidr_ipv4   = each.value
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http_prefix_lists" {
+  for_each = var.internal_albs ? toset(var.web_access_prefix_list_ids) : []
+
+  security_group_id = aws_security_group.dev-site-sg.id
+
+  description    = "Permit HTTP access from ${each.value}"
+  from_port      = 80
+  ip_protocol    = "tcp"
+  prefix_list_id = each.value
+  to_port        = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https_cidrs" {
+  for_each = toset(var.web_access_cidrs)
+
+  security_group_id = aws_security_group.dev-site-sg.id
+
+  description = "Permit HTTPS access from ${each.value}"
+  cidr_ipv4   = each.value
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https_prefix_lists" {
+  for_each = var.internal_albs ? toset(var.web_access_prefix_list_ids) : []
+
+  security_group_id = aws_security_group.dev-site-sg.id
+
+  description    = "Permit HTTPS access from ${each.value}"
+  from_port      = 443
+  ip_protocol    = "tcp"
+  prefix_list_id = each.value
+  to_port        = 443
 }
--- groups/infrastructure/module-albs/variables.tf
+++ groups/infrastructure/module-albs/variables.tf
@@ -3,10 +3,12 @@ variable "stack_name" {
   type        = string
   description = "The name of the Stack / ECS Cluster."
 }
+
 variable "name_prefix" {
   type        = string
   description = "The name prefix to be used for stack / environment name spacing."
 }
+
 
 # Environment
 variable "environment" {
@@ -14,33 +16,45 @@ variable "environment" {
   description = "The environment name, defined in envrionments vars."
 }
 
+
 # Networking
 variable "subnet_ids" {
   type        = list(string)
   description = "Subnet IDs of application subnets from aws-mm-networks remote state."
 }
+
 variable "web_access_cidrs" {
   type        = list(string)
   description = "Subnet CIDRs for web ingress rules in the security group."
 }
+
+variable "web_access_prefix_list_ids" {
+  type        = list(string)
+  description = "A list of prefix list Ids that will have web ingress rules defined in the security group"
+}
+
 variable "vpc_id" {
   type        = string
   description = "The ID of the VPC for the target group and security group."
 }
+
 variable "internal_albs" {
   type        = bool
   description = "Whether the ALBs should be internal or public facing"
 }
+
 
 # DNS
 variable "zone_id" {
   type        = string
   description = "The ID of the hosted zone to contain the Route 53 record."
 }
+
 variable "external_top_level_domain" {
   type        = string
   description = "The type levelel of the DNS domain for external access."
 }
+
 
 # Certificates
 variable "ssl_certificate_id" {
