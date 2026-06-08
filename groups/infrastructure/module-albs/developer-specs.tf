12f3f136555acb6743c4736068ff044ab6bd83cc
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2026-06-08 08:21:46 +0100

    Update SSL policies

--- groups/infrastructure/module-albs/developer-specs.tf
+++ groups/infrastructure/module-albs/developer-specs.tf
@@ -15,7 +15,7 @@ resource "aws_lb_listener" "dev-specs-alb-listener" {
   load_balancer_arn = aws_lb.dev-specs-alb.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = var.ssl_certificate_id
   default_action {
     type = "fixed-response"
