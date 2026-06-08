dd32ecb735f06b1278132930a0916fefcb81788a
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2026-06-08 08:19:46 +0100

    Add prefix list lookups, add supporting variable for Concourse access

--- groups/infrastructure/variables.tf
+++ groups/infrastructure/variables.tf
@@ -3,45 +3,54 @@ variable "environment" {
   type        = string
   description = "The environment name, defined in envrionments vars."
 }
+
 variable "aws_region" {
   default     = "eu-west-2"
   type        = string
   description = "The AWS region for deployment."
 }
+
 variable "aws_profile" {
   default     = "development-eu-west-2"
   type        = string
   description = "The AWS profile to use for deployment."
 }
 
+
 # Terraform
 variable "aws_bucket" {
   type        = string
   description = "The bucket used to store the current terraform state files"
 }
+
 variable "remote_state_bucket" {
   type        = string
   description = "Alternative bucket used to store the remote state files from ch-service-terraform"
 }
+
 variable "state_prefix" {
   type        = string
   description = "The bucket prefix used with the remote_state_bucket files."
 }
+
 variable "deploy_to" {
   type        = string
   description = "Bucket namespace used with remote_state_bucket and state_prefix."
 }
+
 
 # EC2
 variable "ec2_key_pair_name" {
   type        = string
   description = "The key pair for SSH access to ec2 instances in the clusters."
 }
+
 variable "ec2_instance_type" {
   default     = "t3.medium"
   type        = string
   description = "The instance type for ec2 instances in the clusters."
 }
+
 variable "ec2_image_id" {
   default     = "ami-04018f95156d810bc" # ECS optimized Amazon2 Linux in London created 15/03/2023
   type        = string
@@ -54,11 +63,13 @@ variable "asg_max_instance_count" {
   type        = number
   description = "The maximum allowed number of instances in the autoscaling group for the cluster."
 }
+
 variable "asg_min_instance_count" {
   default     = 0
   type        = number
   description = "The minimum allowed number of instances in the autoscaling group for the cluster."
 }
+
 variable "asg_desired_instance_count" {
   default     = 0
   type        = number
@@ -83,11 +94,13 @@ variable "enable_asg_autoscaling" {
   description = "Whether to enable auto-scaling of the ASG by creating a capacity provider for the ECS cluster."
 }
 
+
 # Certificates
 variable "ssl_certificate_id" {
   type        = string
   description = "The ARN of the certificate for https access through the ALB."
 }
+
 
 # DNS
 variable "zone_id" {
@@ -95,17 +108,26 @@ variable "zone_id" {
   type        = string
   description = "The ID of the hosted zone to contain the Route 53 record."
 }
+
 variable "external_top_level_domain" {
   type        = string
   description = "The type levelel of the DNS domain for external access."
 }
 
+
 # Networking
+variable "enable_concourse_access" {
+  type        = bool
+  description = "Defines whether access will be permitted from COncourse (true) or not (false)"
+  default     = false
+}
+
 variable "internal_albs" {
   type        = bool
   description = "Whether the ALBs should be internal or public facing"
   default     = true
 }
+
 
 # Container Insights - ECS
 variable "enable_container_insights" {
